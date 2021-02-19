f6d99a1014b931c6a2df34e0cc2922cbef3535cb
Juan Rusconi <11713137+juanr47@users.noreply.github.com>
2021-02-19 01:17:41 +0000

    Add MissingNumberTest

---
+++ src/test/java/com/jrusco/MissingNumberTest.java
@@ -0,0 +1,38 @@
+package com.jrusco;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+//https://leetcode.com/problems/missing-number/
+public class MissingNumberTest {
+    
+    @Test
+    public void test(){
+        assertEquals(1, missingNumber(new int[]{0}));
+        assertEquals(0, missingNumber(new int[]{1}));
+        assertEquals(2, missingNumber(new int[]{0,1}));
+        assertEquals(0, missingNumber(new int[]{2,1}));
+        assertEquals(2, missingNumber(new int[]{3,0,1}));
+        assertEquals(5, missingNumber(new int[]{1,3,2,0,4}));
+    }
+
+    public int missingNumber(int[] nums) {
+        if (nums.length > 1){
+            Arrays.sort(nums);
+            if (nums[0] != 0){
+                return 0;
+            }
+            for (int index = 0; index < nums.length; index++) {
+                int nextInt = nums[index] + 1;
+                int nextIndex = nums.length > index + 1 ? index + 1 : 0;
+                if (nextInt != nums[nextIndex]){
+                    return nextInt;
+                }
+            }
+        }
+        return nums[0] == 1 ? 0 : 1;
+    }
+}
