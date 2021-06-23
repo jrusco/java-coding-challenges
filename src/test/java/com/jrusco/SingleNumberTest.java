faa7fe837cfa9a72aee1636d17eff42807e0ebdd
Juan Rusconi <11713137+juanr47@users.noreply.github.com>
2021-06-23 11:36:27 +0000

    add SingleNumber challenge solution

---
+++ src/test/java/com/jrusco/SingleNumberTest.java
@@ -0,0 +1,36 @@
+package com.jrusco;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+ */
+public class SingleNumberTest {
+    
+    @Test
+    public void test0(){
+        assertEquals(1, singleNumber(new int[]{1}));
+        assertEquals(-1, singleNumber(new int[]{1,-1,1}));
+        assertEquals(4, singleNumber(new int[]{4,1,2,1,2}));
+    }
+
+    private int singleNumber(int[] nums) {
+        if (nums.length < 2){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != nums[nums.length > i + 1 ? i + 1 : 0]){
+                return nums[i];
+            } else {
+                i = i + 2;
+            }
+        }
+        return nums[0];
+    }
+}
