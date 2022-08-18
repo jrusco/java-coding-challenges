3e43de04c6895d64dc7d3c441e65c368688df799
Juan Rusconi <juan.rusconi@buenbit.com>
2022-08-18 19:42:40 +0000

    Create test for FizzBuzz

---
+++ src/test/java/com/jrusco/leetcode/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.jrusco;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    
+    @Test
+    public void test(){
+        assertTrue(fizzBuzz(0).isEmpty());
+    }
+
+    public List<String> fizzBuzz(int n) {
+        return new ArrayList<>();
+    }
+
+    
+}
